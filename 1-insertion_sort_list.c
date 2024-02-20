95995569476f61ca0e5be03945599016d1bdb51b
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2024-02-20 13:19:03 -0800

    Insertion sort Implementation simplified

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,44 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "sort.h"
+
 /**
- * insertion_sort_list - A function that sorts a list using insertion sort
- * @list: List of integers
- * Returns: A sorted list of integers
+ * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
+ * @h: A pointer to the head of the doubly-linked list.
+ * @n1: A pointer to the first node to swap.
+ * @n2: The second node to swap.
  */
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+{
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*h = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
+}
 
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ *                       using the insertion sort algorithm.
+ * @list: A pointer to the head of a doubly-linked list of integers.
+ *
+ * Description: Prints the list after each swap.
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *curr, *swap, *temp;
 
-/* function to sort a doubly linked list using insertion sort */
-void insertion_sort_list(listint_t **list) {
-
-	listint_t *curr = *list;
-	listint_t *temp = curr->next;
-
-	if (*list == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	while (curr != NULL) {
-		if (curr->next != NULL && curr->next->n < curr->n){
-			temp = curr->next;
-
-			curr->next = temp->next;
-
-			if (temp->next != NULL){
-				temp->next->prev = curr;
-			}
-			temp->prev = curr->prev;
-
-			if (curr->prev != NULL){
-                                curr->prev->next = temp;
-                        }
-			else{         /*MuST ADD DO-WHILE SEGMENT PLUS BOOLEAN FOR SORTEDNESS*/
-				*list = temp;
-			}
-			temp->next = curr;
-			curr->prev = temp;
+	for (curr = (*list)->next; curr != NULL; curr = temp)
+	{
+		temp = curr->next;
+		swap = curr->prev;
+		while (swap != NULL && curr->n < swap->n)
+		{
+			swap_nodes(list, &swap, curr);
+			print_list((const listint_t *)*list);
 		}
-		print_list(*list);
-                curr = curr->next;
 	}
-	printf("\n");
 }
--- sort.h
+++ sort.h
@@ -14,6 +14,7 @@ typedef struct listint_s
 /*UTILITIES*/
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2);
 
 /*ALGOs*/
 void bubble_sort(int *array, size_t size);
