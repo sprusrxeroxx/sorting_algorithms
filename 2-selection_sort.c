e4bd95d6483b7cdbdb473a1db2e5c4e04580bf40
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2024-02-20 15:22:19 -0800

    Exceptions

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -15,7 +15,7 @@ void selection_sort(int *array, size_t size)
 	int temp;
 	size_t i, j, min_index;
   /* Base case: empty or single-element array */
-	if (size <= 1)
+	if (size < 2 || array == NULL)
 		return;
 
 	for (i = 0; i < size - 1; ++i)
