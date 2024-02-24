1df0f4a1246be03aa27814bbbd63d60c1004f2ee
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2024-02-24 13:51:00 -0800

    Betty fix

--- sort.h
+++ sort.h
@@ -3,6 +3,14 @@
 
 #include <stddef.h>
 
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+
 typedef struct listint_s
 {
 	const int n;
