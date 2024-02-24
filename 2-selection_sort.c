8c13bd903f2336d0598b5a65c3c35ff96984c8e2
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2024-02-24 13:19:29 -0800

    Quick Sort optimization

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -14,6 +14,9 @@ void selection_sort(int *array, size_t size)
 {
 	int temp;
 	size_t i, j, min_index;
+	bool swapped;
+	
+	swapped = false;
   /* Base case: empty or single-element array */
 	if (size < 2 || array == NULL)
 		return;
@@ -21,14 +24,27 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; ++i)
 	{
 		min_index = i;
+		swapped = false;
 		for (j = i + 1; j < size; ++j)
 		{
 			if (array[j] < array[min_index])
+			{
 				min_index = j;
+				swapped = true;
+			}
+				
 		}
-	temp = array[i];
-	array[i] = array[min_index];
-	array[min_index] = temp;
-	print_array(array, size);
+		if (! swapped){
+			break;
+		}
+		else
+		{
+			if (i != min_index){
+				temp = array[i];
+				array[i] = array[min_index];
+				array[min_index] = temp;
+				print_array(array, size);
+			}
+		}
 	}
 }
