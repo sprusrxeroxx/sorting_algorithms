204ac31bc8ce1d69a3065de47948106f5656824e
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2024-02-24 13:46:49 -0800

    Selection Sort Optimization

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -15,37 +15,27 @@ void selection_sort(int *array, size_t size)
 {
 	int temp;
 	size_t i, j, min_index;
-	bool swapped;
-	
-	swapped = false;
   /* Base case: empty or single-element array */
 	if (size < 2 || array == NULL)
 		return;
 
-	for (i = 0; i < size - 1; ++i)
+	for (i = 0; i < size; ++i)
 	{
 		min_index = i;
-		swapped = false;
 		for (j = i + 1; j < size; ++j)
 		{
 			if (array[j] < array[min_index])
 			{
 				min_index = j;
-				swapped = true;
 			}
 				
 		}
-		if (! swapped){
-			break;
-		}
-		else
+		if (i != min_index)
 		{
-			if (i != min_index){
-				temp = array[i];
-				array[i] = array[min_index];
-				array[min_index] = temp;
-				print_array(array, size);
-			}
+			temp = array[i];
+			array[i] = array[min_index];
+			array[min_index] = temp;
+			print_array(array, size);
 		}
 	}
 }
